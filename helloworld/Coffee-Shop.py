5b69d99e9915d7aba66a2d8ebf37e32895ac7a16
unknown <alvinjoyner3@gmail.com>
2025-09-06 15:41:49 -0400

    Adding my first Python Project!!!

---
+++ helloworld/Coffee-Shop.py
@@ -0,0 +1,9 @@
+#hello Welcome to my Coffee and yt university learned python
+print("Hello! Welcome to my coffee shop!!")
+name = input("What is your name? ")
+print("Helloo " + name + " Thank you for comining in today! Take a look at the menu, and lmk what looks good today :)"
+"")
+menu = " Rice n Coffee Latte, Espresso Martini, Midnight Black, Rawtid Ripe Mangoe Ginger Shot "
+print(menu)
+order = input()
+print("Heard that, ill get started now and I'll let you know when it is ready! In the mean time," + name + " Ima take the "  + order + " and make sure I put my elbow in there")
